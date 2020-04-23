cc0f119dc254d7f895d15a6eea6dffe2ce85f7bb
Andrey Galeazzi <andrey_hp@hotmail.com>
2020-04-23 11:01:19 -0300

    - border changes if user types correctly and vice-versa;

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,70 @@
+let tempoInicial = $('#tempo-digitacao').text();
+let campo = $('.campo-digitacao');
 
-let frase = $('.frase').text();
-let numPalavras = frase.split(' ').length;
-let tamanhoFrase = $('#tamanho-frase');
+$(() => {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    inicializaMarcadores();
+    $('#botao-reiniciar').click(reiniciaJogo);
+});
 
-tamanhoFrase.text(numPalavras)
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on('input', function () {
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(" ").length;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-char").text(qtdCaracteres);
+    });
+}
+
+function inicializaCronometro() {
+    let tempoRestante = $('#tempo-digitacao').text();
+    campo.one('focus', () => {
+        $('#botao-reiniciar').attr('disabled', true);
+        let id = setInterval(() => {
+            tempoRestante--;
+            $('#tempo-digitacao').text(tempoRestante);
+
+            if (tempoRestante < 1) {
+                campo.attr('disabled', true);
+                clearInterval(id)
+                $('#botao-reiniciar').attr('disabled', false);
+                campo.addClass('campo-desativado');
+            }
+        }, 1000);
+    });
+}
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val('');
+    $("#contador-palavras").text('0');
+    $("#contador-caracteres").text('0');
+    $('#tempo-digitacao').text(tempoInicial);
+    inicializaCronometro()
+    campo.removeClass('campo-desativado');
+    campo.removeClass('borda-vermelha');
+    campo.removeClass('borda-verde');
+}
+
+function inicializaMarcadores() {
+    let frase = $('.frase').text();
+    campo.on('input', () => {
+        let digitado = campo.val();
+        let ehCorreto = frase.startsWith(digitado);
+
+        campo.toggleClass('borda-verde', ehCorreto);
+        campo.toggleClass('borda-vermelha', !ehCorreto);
+    });
+}
